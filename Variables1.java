c06d8d2b5000f86d179c53899305e83b86adfb48
VICTORJSV24 <143622557+VICTORJSV24@users.noreply.github.com>
2023-09-06 08:40:16 -0600

    Add files via upload

---
+++ Variables1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Variables1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;
+        String nombre = "Victor";
+        String materia = "Fundamentos de programacion";
+        String nombreMascota = "Gatito";
+        final int anioNacimiento = 2000;
+       
+        String cadenaTexto = "cadena de texto!!!%%$$$";
+       boolean prenderLuz= true;
+       boolean apagarLuz= false;
+       edad= 25;
+       edad= 50;
+        
+        System.out.println("Hola mi nombre es:"+ nombre);
+        System.out.println("Hola mi nombre es:" + nombre + " y estudio" + materia);
+        
+        
+                
+        
+    }
+    
+}
