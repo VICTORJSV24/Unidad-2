8f4210545e80b0db5a5c32c0e4f0c87b59a9ff94
VICTORJSV24 <143622557+VICTORJSV24@users.noreply.github.com>
2023-09-08 08:38:25 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int base = 6;
+        int altura =8;
+        int area= (base*altura)/2;
+        System.out.println("el area es" +area);
+    }
+    
+}
